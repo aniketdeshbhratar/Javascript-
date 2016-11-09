412af9eb448a25009d7f79aa50ad9325ae9920ad
Aniket <aniketdeshbhratar.09@gmail.com>
2016-11-09 12:12:23 +0530

    Update widget.js

--- widget.js
+++ widget.js
@@ -16,44 +16,7 @@ function closePopup() {
 
 
 //adding <style>
-document.write("<style>
-               .popup{
-    position:absolute;
-    top:0px;
-    left:0px;
-    margin:100px auto;
-    width:200px;
-    height:150px;
-    font-family:verdana;
-    font-size:13px;
-    padding:10px;
-    background-color:rgb(240,240,240);
-    border:2px solid grey;
-    z-index:100000000000000000;
-    display:none
-    }
-    
-.cancel{
-    display:relative;
-    cursor:pointer;
-    margin:0;
-    float:right;
-    height:10px;
-    width:14px;
-    padding:0 0 5px 0;
-    background-color:red;
-    text-align:center;
-    font-weight:bold;
-    font-size:11px;
-    color:white;
-    border-radius:3px;
-    z-index:100000000000000000;
-    }
-
-.cancel:hover{
-    background:rgb(255,50,50);
-    }
-               </style>");
+document.write("<style>.popup{position:absolute;top:0;left:0;margin:100px auto;width:200px;height:150px;font-family:verdana;font-size:13px;padding:10px;background-color:#f0f0f0;border:2px solid grey;z-index:1.0E+17;display:none}.cancel{display:relative;cursor:pointer;margin:0;float:right;height:10px;width:14px;padding:0 0 5px;background-color:red;text-align:center;font-weight:700;font-size:11px;color:#fff;border-radius:3px;z-index:1.0E+17}.cancel:hover{background:#ff3232}</style>");
 
 
 
