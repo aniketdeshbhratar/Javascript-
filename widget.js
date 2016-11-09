cd1e5facfcf29b8efb29eef79adc8fb0f73ff42b
Aniket <aniketdeshbhratar.09@gmail.com>
2016-11-09 12:15:21 +0530

    Update widget.js

--- widget.js
+++ widget.js
@@ -5,6 +5,15 @@ wid.style.width="400px";
 wid.style.background="black";
 
 document.getElementById("widget").appendChild(wid);
+
+var element = document.createElement("input");
+  //Assign different attributes to the element. 
+  element.type = button;
+  element.value = Open; // Really? You want the default value to be the type string?
+  element.name = open; // And the name too?
+  element.onclick = openPopup();
+  //Append the element in page (in span).  
+  wid.appendChild(element);
 //open popup
 function openPopup() {
     document.getElementById('test').style.display = 'block';
