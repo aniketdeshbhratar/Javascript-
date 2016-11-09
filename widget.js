7d2309c59e104bc8c8232a28f522366de651cec2
Aniket <aniketdeshbhratar.09@gmail.com>
2016-11-09 11:30:38 +0530

    Create widget.js

---
+++ widget.js
@@ -0,0 +1,13 @@
+
+var wid = document.createElement("div");
+wid.style.height="400px";
+wid.style.width="400px";
+wid.style.background="black";
+wid.innerHTML = "hey";
+document.getElementById("widget").appendChild(wid);
+
+
+
+
+
+
